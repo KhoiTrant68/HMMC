92db31de8516abcacb57a2f88bc32537788a70d4
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2025-12-30 16:20:23 +0700

    update

--- vmamba/setup.py
+++ vmamba/setup.py
@@ -43,7 +43,7 @@ def get_cuda_bare_metal_version(cuda_dir):
     return raw_output, bare_metal_version
 
 # MODES = ["oflex"]
-MODES = ["core", "ndstate", "oflex"]
+MODES = ["core", "oflex"]
 # MODES = ["core", "ndstate", "oflex", "nrow"]
 
 def get_ext():
