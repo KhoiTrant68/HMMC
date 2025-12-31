bac591c6be371786c693f226e5829f9371e1682a
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2025-12-31 14:20:04 +0700

    update test

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -11,7 +11,7 @@ with open("scripts/openimage.yaml", "r") as f:
 
 fo.config.dataset_zoo_dir = data["path"]
 
-map_amount = {"train": 20000, "validation": 1000}
+map_amount = {"train": 1000, "validation": 100}
 name = "open-images-v7"
 
 OUTPUT_ROOT = "./dataset"
